2ca0c111e16b7ef4bb293d446d8bcbb9eeeabda0
Joe Bissonnette <z3nder@gmail.com>
2015-07-02 09:34:46 -0400

    Fix Basket movement after engine Sprite fix

--- Peggle/Basket.cpp
+++ Peggle/Basket.cpp
@@ -23,25 +23,15 @@ void Basket::Start()
 
 void Basket::Update()
 {
+	float dt = gTimer->GetDeltaTime();
 	D3DXVECTOR3 currentPos = GetPosition();
 
-	////gDInput->keyDown(DIK_)
-	//if (this->GetPosition().x >= 300.0f)
-	//{
-	//	 speed = -1;
-	//}
-	//
-	//if (this->GetPosition().x <= -450.0f)
-	//{
-	//	speed = 1;
-	//}
-	//
-
-	float dt = gTimer->GetDeltaTime();
-	currentPos.x += speed * dt;
-
-	std::cout << "X: " << currentPos.x << "Y: " << currentPos.y << std::endl;
+	if (this->GetPosition().x >= 300.0f || this->GetPosition().x <= -450.0f)
+	{
+		 speed *= -1;
+	}
 	
+	currentPos.x += speed * dt;
 	SetPosition(currentPos.x, currentPos.y);
 }
 
